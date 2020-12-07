5580db3b41229b06569a082a250e6da30a6bc968
lay <2491812016@qq.com>
2020-12-07 17:50:43 +0800

    新增第一章内容

---
+++ app/src/main/java/com/lay/designpatterms/EasyFoctory/Calculator.java
@@ -0,0 +1,15 @@
+package com.lay.designpatterms.EasyFoctory;
+
+import java.util.Scanner;
+
+//Demo 1 - 计算器程序 - 12 - 7
+// 理解封装/继承/多态
+public class Calculator {
+    private static String inputA = "请输入数字A：";
+    private static String inputB = "请输入数字B：";
+    private static String inputCal = "请输入运算符号（+ 、 - 、* 、 /）：";
+
+    public static void main(String[] args) {
+
+    }
+}
--- app/src/main/java/com/lay/designpatterms/MainActivity.kt
+++ app/src/main/java/com/lay/designpatterms/MainActivity.kt
@@ -2,10 +2,19 @@ package com.lay.designpatterms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initData()
     }
+
+    private fun initData() {
+        val scanner = Scanner(System.`in`)
+        scanner.hasNextDouble()
+    }
+
+
 }
